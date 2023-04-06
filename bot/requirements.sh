5b339869fbcabd581ea905a8818e94189405b335
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2023-04-07 00:33:31 +0800

    chore(deps): create requirements.sh

---
+++ bot/requirements.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+pip_path="/Users/hermeschen/.pyenv/shims/pip"
+${pip_path} install -U pip wheel setuptools pip-review
+${pip_path} install -U jupyter
+${pip_path} install -U polars datasets
+${pip_path} install -U fasttext
+${pip_path} install -U https://github.com/Lightning-AI/lightning/archive/refs/heads/master.zip
+${pip_path} install -U --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
