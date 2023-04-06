b17b78468b54f2cd3b59fe041430607bb5aac8d7
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2023-04-07 00:34:05 +0800

    feat(data module): load dataset

---
+++ bot/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+B
+model/interface.py,2/4/24f95ef24b65382c625493853ee19d0c730b5037
+@
+data/__init__.py,b/8/b88f171bae697b26f07f00ce52e19fd14a29dc6b
+A
+model/__init__.py,8/5/856bddc8fd3cefd6e9499ea45d0f52219e17a7e2
--- bot/data/interface.py
+++ bot/data/interface.py
@@ -1,10 +1,26 @@
+from typing import Any
+
 import pytorch_lightning as pl
+from datasets import load_dataset
 
 
 class Interface(pl.LightningDataModule):
     def __init__(self):
+        self.train_dataset: Any = None
+        self.validation_dataset: Any = None
+        self.test_dataset: Any = None
+
+    def prepare_data(self) -> None:
+        load_dataset("daily_dialog", download_mode="force_redownload", num_proc=8)
 
     def setup(self, stage: str) -> None:
+        if stage == "fit":
+            self.train_dataset, self.validation_dataset = load_dataset("daily_dialog",
+                                                                       split=["train", "validation"],
+                                                                       num_proc=8)
+
+        if stage == "test":
+            self.test_dataset = load_dataset("daily_dialog", split="test", num_proc=8)
 
     def train_dataloader(self) -> TRAIN_DATALOADERS:
 
