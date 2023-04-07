cc18c33479acbe0701235e4d6d63b96d56ff880c
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2023-04-07 14:31:16 +0800

    chore(deps): update requirements.sh

--- bot/requirements.sh
+++ bot/requirements.sh
@@ -1,8 +1,15 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 pip_path="$HOME/.pyenv/shims/pip"
 ${pip_path} install -U pip wheel setuptools pip-review
 ${pip_path} install -U jupyter
-${pip_path} install -U polars datasets
+${pip_path} install -U polars datasets tensorboard
 ${pip_path} install -U fasttext
 ${pip_path} install -U https://github.com/Lightning-AI/lightning/archive/refs/heads/master.zip
-${pip_path} install -U --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
+case $(uname) in
+    "Darwin")
+        ${pip_path} install -U --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/cpu
+        ;;
+    "Linux")
+        ${pip_path} install -U --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/cu118
+        ;;
+esac
