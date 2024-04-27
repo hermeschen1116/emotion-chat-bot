64b5f2f0e8f11385458324be14dc7d9ba920dd86
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-04-27 23:59:25 +0900

    perf: set new option to pipeline

--- src/bot/emotion_model/libs/DataProcess.py
+++ src/bot/emotion_model/libs/DataProcess.py
@@ -8,5 +8,13 @@ def generate_dummy_representation(target_emotion: int) -> torch.tensor:
             return dummy
 
 
-def get_sentiment_composition(sentiment_composition: list) -> torch.tensor:
-    return torch.softmax(torch.tensor([result["score"] for result in sentiment_composition]), dim=-1)
+def get_sentiment_composition(analysis_result: list) -> torch.tensor:
+    sentiment_possibility: dict = {}
+    for emotion_score in analysis_result[0]:
+        values: list = list(emotion_score.values())
+        sentiment_possibility[values[0]] = values[1]
+
+    emotions: list = ["neutral", "anger", "disgust", "fear", "happiness", "sadness", "surprise"]
+    sentiment_composition: list = [sentiment_possibility[emotion] for emotion in emotions]
+
+    return torch.softmax(torch.tensor(sentiment_composition), dim=-1, dtype=torch.float32)
