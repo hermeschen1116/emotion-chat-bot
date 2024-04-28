2eb7547dbf367ba0949fd80f3741eb5f7cade9aa
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-04-28 17:48:20 +0900

    fix: fix type problem when saving result

--- src/bot/emotion_model/evaluate.py
+++ src/bot/emotion_model/evaluate.py
@@ -2,8 +2,6 @@ from dataclasses import dataclass
 from typing import Any, Optional
 
 import torch
-from tqdm.auto import tqdm
-
 import wandb
 from datasets import load_from_disk
 from torcheval.metrics.functional import multiclass_f1_score, multiclass_accuracy
@@ -40,13 +38,13 @@ model = EmotionModel(wandb_args.config["attention_type"], dtype=args.dtype, devi
 
 eval_dataset = eval_dataset.map(lambda samples: {
     "bot_representation": [model.representation_evolution(sample[0], sample[1])
-                            for sample in zip(samples["bot_representation"],
-                                              samples["user_dialog_emotion_composition"])]
+                           for sample in zip(samples["bot_representation"],
+                                             samples["user_dialog_emotion_composition"])]
 }, batched=True)
 
 eval_dataset = eval_dataset.map(lambda samples: {
     "bot_most_possible_emotion": [[torch.argmax(torch.tensor(representation)) for representation in sample]
-                                   for sample in samples]
+                                  for sample in samples]
 }, input_columns="bot_representation", batched=True, num_proc=16)
 
 emotion_pred: torch.tensor = torch.tensor([emotion for sample in eval_dataset["bot_most_possible_emotion"]
@@ -62,13 +60,14 @@ wandb.log({
 emotion_labels: list = ["neutral", "anger", "disgust", "fear", "happiness", "sadness", "surprise"]
 eval_dataset = eval_dataset.map(lambda samples: {
     "bot_most_possible_emotion": [[emotion_labels[emotion_id] for emotion_id in sample[1:]]
-                                   for sample in samples["bot_most_possible_emotion"]],
+                                  for sample in samples["bot_most_possible_emotion"]],
     "bot_emotion": [[emotion_labels[emotion_id] for emotion_id in sample] for sample in samples["bot_emotion"]]
 }, batched=True, num_proc=16)
 
-result: list
-for i, sample in tqdm(enumerate(eval_dataset)):
-
+result = eval_dataset.map(lambda samples: {
+    "bot_most_possible_emotion": [", ".join(sample) for sample in samples["bot_most_possible_emotion"]],
+    "bot_emotion": [", ".join(sample) for sample in samples["bot_emotion"]]
+}, remove_columns=["bot_representation", "bot_dialog", "user_dialog"], batched=True, num_proc=16)
 
 wandb.log({"evaluation_result": wandb.Table(dataframe=result.to_pandas())})
 
