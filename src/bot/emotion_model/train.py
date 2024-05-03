e6481683fcdbec677907f873324f0913d091c87a
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-05-03 19:28:05 +0900

    feat: add code to save model

--- src/bot/emotion_model/train.py
+++ src/bot/emotion_model/train.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from typing import Any, Optional
@@ -6,6 +8,7 @@ import torch
 import torch.nn.functional as f
 from datasets import load_from_disk
 from torch.utils.data import DataLoader
+from torcheval.metrics.functional import multiclass_f1_score, multiclass_accuracy
 from tqdm.auto import tqdm
 from transformers.hf_argparser import HfArgumentParser, HfArg
 
@@ -15,12 +18,13 @@ from libs.CommonConfig import (
     CommonScriptArguments,
     get_torch_device,
 )
-from libs.EmotionModel import EmotionModel
+from libs.EmotionModel import EmotionModel, representation_evolute
 
 
 @dataclass
 class ScriptArguments(CommonScriptArguments):
     dataset: Optional[str] = HfArg(aliases="--dataset", default="daily_dialog_for_EM:latest")
+    path_to_save_model: Optional[str] = HfArg(aliases="--path-to-save-model", default="./model")
     dtype: Optional[Any] = HfArg(aliases="--dtype", default=torch.float32)
     device: Optional[str] = HfArg(aliases="--device", default_factory=get_torch_device)
 
@@ -47,15 +51,14 @@ dataset_path = run.use_artifact(args.dataset).download()
 dataset = load_from_disk(dataset_path)
 
 model = EmotionModel(wandb.config["attention_type"])
-model = torch.compile(model)
 
 loss_function = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.Adam(model.parameters(), lr=wandb.config["learning_rate"])
 
 train_dataloader = DataLoader(dataset["train"])
 validation_dataloader = DataLoader(dataset["validation"])
-
 for i in range(wandb.config["num_epochs"]):
+    running_loss: torch.float = 0
     model.train()
     for sample in tqdm(train_dataloader, colour="green"):
         representation, emotion_composition = sample["bot_representation"], sample["user_dialog_emotion_composition"]
@@ -63,12 +66,20 @@ for i in range(wandb.config["num_epochs"]):
 
         optimizer.zero_grad()
 
-        output = model.representation_evolute(representation, emotion_composition)
+        output = representation_evolute(model, representation, emotion_composition)
 
         loss = loss_function(output, labels)
+        wandb.log({"train/loss": loss.item()})
+        running_loss += loss.item()
+
         loss.backward()
         optimizer.step()
 
+    wandb.log({"train/train_loss": running_loss / len(train_dataloader)})
+
+    running_loss: torch.float = 0
+    true_labels: list = []
+    predicted_labels: list = []
     model.eval()
     with torch.no_grad():
         for sample in tqdm(validation_dataloader, colour="blue"):
@@ -76,9 +87,30 @@ for i in range(wandb.config["num_epochs"]):
                 "user_dialog_emotion_composition"]
             labels = f.one_hot(torch.cat(sample["bot_emotion"]), 7).to(dtype=torch.float)
 
-            output = model.representation_evolute(representation, emotion_composition)
+            output = representation_evolute(model, representation, emotion_composition)
 
             loss = loss_function(output, labels)
+            wandb.log({"val/loss": loss.item()})
+            running_loss += loss.item()
+            true_labels += sample["bot_emotion"]
+            predicted_labels.append(torch.argmax(output, dim=1))
 
+        wandb.log({"val/val_loss": running_loss / len(validation_dataloader)})
+        wandb.log({
+            "val/f1_score": multiclass_f1_score(torch.cat(true_labels), torch.cat(predicted_labels),
+                                                num_classes=7, average="weighted"),
+            "val/accuracy": multiclass_accuracy(torch.cat(true_labels), torch.cat(predicted_labels),
+                                                num_classes=7)
+        })
+
+model_artifact = wandb.Artifact(wandb.config["trained_model_name"], type="model")
+
+model = torch.compile(model)
+torch.save(model.state_dict(), args.path_to_save_model)
+model_artifact.add_file(args.path_to_save_model)
+run.log_artifact(model_artifact)
 
 wandb.finish()
+
+if os.path.exists(args.path_to_save_model):
+    shutil.rmtree(args.path_to_save_model, ignore_errors=True)
