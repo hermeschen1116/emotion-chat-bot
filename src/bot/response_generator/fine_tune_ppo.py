5068c44dcaf3ea038f48f5682a08748d11637961
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-07-15 13:23:44 +0900

    feat: check reward function input and output type

--- src/bot/response_generator/fine_tune_ppo.py
+++ src/bot/response_generator/fine_tune_ppo.py
@@ -17,7 +17,7 @@ from transformers.hf_argparser import HfArg
 from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer
 from unsloth import FastLanguageModel
 
-from libs import CommonScriptArguments, CommonWanDBArguments, ResponseGeneratorPipeline
+from libs import CommonScriptArguments, CommonWanDBArguments
 
 
 @dataclass
@@ -52,7 +52,7 @@ wandb.config["special_tokens"] = chat_template["special_tokens"]
 # Load Dataset
 dataset = load_dataset("hermeschen1116/daily_dialog_for_RG", num_proc=16, trust_remote_code=True)
 dataset = concatenate_datasets([dataset["train"], dataset["validation"]])
-# dataset = dataset.train_test_split(train_size=0.001)["train"]   # use very small dataset to debuG
+# dataset = dataset.train_test_split(train_size=0.05)["train"]   # use very small dataset to debuG
 
 history_length: int = 2 * wandb.config["num_turns_history"]
 dataset = dataset.filter(lambda sample: len(sample) >= (2 + history_length), input_columns="prompt", num_proc=16)
@@ -141,9 +141,10 @@ sentiment_analysis_model = torch.compile(analyser.model)
 
 
 # [TODO] a reward function contain length and emotion
-def reward(batch) -> float:
+def reward(batch: dict) -> list:
 	print("Hello Huston, here is reward function")
-	return 1.0
+
+	return [1.0] * len(batch["label"])
 
 
 ppo_config = PPOConfig(
@@ -198,15 +199,15 @@ for epoch in trange(wandb.config["num_epochs"], colour="blue"):
 		response_tensors = tuner.generate(
 			query_tensors,
 			return_prompt=False,
-			batch_size=1,   # must set to 1 if using streamer
-			streamer=streamer,  # use streamer to show the generation process
+			# batch_size=1,   # must set to 1 if using streamer
+			# streamer=streamer,  # use streamer to show the generation process
 			**generation_config.to_dict()
 		)
 		batch["response"] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
 
 		# Compute reward score
-		pipe_outputs = reward(batch)
-		rewards = [torch.tensor(output[1]["score"]) for output in pipe_outputs]
+		reward_scores = reward(batch)
+		rewards = [torch.tensor(scores, dtype=torch.float) for scores in reward_scores]
 
 		# Run PPO step
 		stats = tuner.step(query_tensors, response_tensors, rewards)
