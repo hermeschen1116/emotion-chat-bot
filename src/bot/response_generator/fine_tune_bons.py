be6578f802427da4e3717e0dae66706a989b4695
yangyx30678 <yangyx30678@gmail.com>
2024-07-22 18:16:57 +0800

    WIP: now works for ref score

--- src/bot/response_generator/fine_tune_bons.py
+++ src/bot/response_generator/fine_tune_bons.py
@@ -8,12 +8,11 @@ from datasets import load_dataset
 from peft.peft_model import PeftModel
 from torch import tensor
 from tqdm.auto import tqdm
-from transformers import (BitsAndBytesConfig, GenerationConfig, HfArgumentParser, pipeline, TextStreamer,AutoModelForSequenceClassification)
+from transformers import (GenerationConfig, HfArgumentParser, pipeline, TextStreamer)
 from transformers.hf_argparser import HfArg
 from trl import AutoModelForCausalLMWithValueHead
-from trl.trainer.rloo_trainer import  RLOOConfig, RLOOTrainer
+from torch.utils.data import DataLoader
 from trl.core import LengthSampler
-from trl.extras import BestOfNSampler
 from unsloth import FastLanguageModel
 
 from libs import CommonScriptArguments, CommonWanDBArguments
@@ -136,75 +135,72 @@ sentiment_analyser = pipeline(
 	trust_remote_code=True
 )
 
-# # Detect gibberish
-# gibberish_analyser = pipeline(
-# 	model=wandb.config["gibberish_detector_model"],
-# 	tokenizer=wandb.config["gibberish_detector_model"],
-# 	max_length=512,
-# 	framework="pt",
-# 	task="text-classification",
-# 	num_workers=16,
-# 	device_map="cpu",
-# 	torch_dtype="auto",
-# 	model_kwargs={
-# 		# "quantization_config": BitsAndBytesConfig(
-# 		# 	load_in_4bit=True,
-# 		# 	bnb_4bit_compute_dtype=torch.float16
-# 		# ),
-# 		"low_cpu_mem_usage": True
-# 	},
-# 	trust_remote_code=True
-# )
+# Detect gibberish
+gibberish_analyser = pipeline(
+	model=wandb.config["gibberish_detector_model"],
+	tokenizer=wandb.config["gibberish_detector_model"],
+	max_length=512,
+	framework="pt",
+	task="text-classification",
+	num_workers=16,
+	device_map="cpu",
+	torch_dtype="auto",
+	model_kwargs={
+		# "quantization_config": BitsAndBytesConfig(
+		# 	load_in_4bit=True,
+		# 	bnb_4bit_compute_dtype=torch.float16
+		# ),
+		"low_cpu_mem_usage": True
+	},
+	trust_remote_code=True
+)
+
+def emotion_reward(response: str, emotion: str) -> float:
+	score = sentiment_analyser(response)[0]
+	if score["label"] == emotion:
+		return score["score"] * 10
+	else:
+		return score["score"] - 1
 
 
-# def emotion_reward(response: str, emotion: str) -> float:
-# 	score = sentiment_analyser(response)[0]
+def non_gibberish_reward(response: str) -> float:
+	score = gibberish_analyser(response)[0]
 
-# 	if score["label"] == emotion:
-# 		return score["score"] * 10
-# 	else:
-# 		return score["score"] - 1
-
-
-# def non_gibberish_reward(response: str) -> float:
-# 	score = gibberish_analyser(response)[0]
-
-# 	match score["label"]:
-# 		case "clean":
-# 			return score["score"] * 10
-# 		case "mild gibberish":
-# 			return score["score"] * 0.9
-# 		case _:
-# 			return score["score"] - 1
+	match score["label"]:
+		case "clean":
+			return score["score"] * 10
+		case "mild gibberish":
+			return score["score"] * 0.9
+		case _:
+			return score["score"] - 1
 
 
-# # [TODO] 用級距的方式來給予分數
-# def length_reward(response_length: int) -> float:
-# 	difference_ratio_min = (response_length - wandb.config["min_new_tokens"]) / wandb.config["min_new_tokens"]
-# 	difference_ratio_max = (response_length - wandb.config["max_new_tokens"]) / wandb.config["max_new_tokens"]
+# [TODO] 用級距的方式來給予分數
+def length_reward(response_length: int) -> float:
+	difference_ratio_min = (response_length - 5) / 5
+	difference_ratio_max = (response_length - 20) / 20
 
-# 	if abs(difference_ratio_min) < 1:
-# 		return difference_ratio_min * 0.0001
-# 	elif abs(difference_ratio_min) > 1 > abs(difference_ratio_max):
-# 		return abs(difference_ratio_min + difference_ratio_max) * 10
-# 	else:
-# 		return difference_ratio_max * 0.9
+	if abs(difference_ratio_min) < 1:
+		return difference_ratio_min * 0.0001
+	elif abs(difference_ratio_min) > 1 > abs(difference_ratio_max):
+		return abs(difference_ratio_min + difference_ratio_max) * 10
+	else:
+		return difference_ratio_max * 0.9
 
 
-# def reward(batch: dict) -> list:
-# 	emotion_scores = [emotion_reward(response, emotion)
-# 	                  for response, emotion in zip(batch["response"], batch["label"])]
-# 	length_scores = [length_reward(response_length) for response_length in batch["response_length"]]
-# 	gibberish_scores = [non_gibberish_reward(response) for response in batch["response"]]
+def reward(batch: dict) -> list:
+	emotion_scores = [emotion_reward(response, emotion)
+	                  for response, emotion in zip(batch["response_ref"], batch["label"])]
+	length_scores = [length_reward(response_length) for response_length in batch["response_ref_len"]]
+	gibberish_scores = [non_gibberish_reward(response) for response in batch["response_ref"]]
 
-# 	reward_weight = tensor(wandb.config["reward_weights"], dtype=torch.float)
-# 	reward_bias = tensor(wandb.config["reward_bias"], dtype=torch.float)
-# 	return [reward_weight.dot(tensor(reward_score, dtype=torch.float)) + reward_bias
-# 	        for reward_score in zip(emotion_scores, length_scores, gibberish_scores)]
+	reward_weight = tensor([0.4, 0.25, 0.35], dtype=torch.float)
+	reward_bias = tensor(0.001, dtype=torch.float)
+	return [reward_weight.dot(tensor(reward_score, dtype=torch.float)) + reward_bias
+	        for reward_score in zip(emotion_scores, length_scores, gibberish_scores)]
 
 # [TODO] somehow if I comment this it stops working
 optimizer = PagedLion32bit(filter(lambda p: p.requires_grad, ppo_model.parameters()), lr=wandb.config["max_new_tokens"])
-# lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=wandb.config["lr_gamma"])
 length_sampler = LengthSampler(wandb.config["min_new_tokens"], wandb.config["max_new_tokens"])
 
 streamer = TextStreamer(
@@ -232,18 +228,20 @@ device = 0 if torch.cuda.is_available() else "cpu"
 
 # :: [Resp]
 response_tensors_ref, response_tensors = [], []
+response_tensors_ref_len, response_tensors_len = [], []
 response_tensors_ans = []
 # :: [[Resp]]
 response_tensors_best_of = []
+response_tensors_best_of_len = []
 
-gen_kwargs = {"min_length": -1, "top_k": 0.0, "top_p": 1.0, "do_sample": True, "pad_token_id": tokenizer.eos_token_id}
+# gen_kwargs = {"min_length": -1, "top_k": 0.0, "top_p": 1.0, "do_sample": True, "pad_token_id": tokenizer.eos_token_id}
 
 query_tensors = [input_ids.squeeze(0) for input_ids in dataset["input_ids"]]
 
 for i in range(len(dataset)):
     gen_len = length_sampler()
     query = query_tensors[i]
-    input = tokenizer.decode(query)
+    input = tokenizer.decode(query, skip_special_tokens=True)
     
     output = ppo_model.generate(
         query.unsqueeze(dim=0).to(device),
@@ -251,11 +249,9 @@ for i in range(len(dataset)):
         generation_config=generation_config,
 		streamer=streamer,  # use streamer to show the generation process
         ).squeeze()
-    response_tensors_ref.append(tokenizer.decode(output)[len(input):])
-    
-
-    # output = model.generate(query.unsqueeze(dim=0).to(device), max_new_tokens=gen_len, **gen_kwargs).squeeze()
-    # response_tensors.append(tokenizer.decode(output))
+    response = tokenizer.decode(output, skip_special_tokens=True)[len(input):]
+    response_tensors_ref.append(response)
+    response_tensors_ref_len.append(len(response))
 
     # generating copies of the same query for the Best-of-n sampling
     queries = query.repeat((N_BEST_OF, 1))
@@ -266,36 +262,33 @@ for i in range(len(dataset)):
         # batch_size=1,
 		# streamer=streamer,  # use streamer to show the generation process
         ).squeeze()
-    response_tensors_best_of.append([text[len(input):] for text in tokenizer.batch_decode(output, skip_special_tokens=False)]
-)
+    responses = [text[len(input):] for text in tokenizer.batch_decode(output, skip_special_tokens=True)]
+    response_tensors_best_of.append(responses)
+    response_tensors_best_of_len.append([len(response) for response in responses])
     
-scores_best_of = []
-label_best_of = []
-for i, response in enumerate(response_tensors_best_of):
-    # print(response)
-    # print(sentiment_analyser(response))
-    for output in sentiment_analyser(response):
-        print(output)
-    print()
-    # base_score = scores_ref[i]
-    scores_best_of.append(torch.tensor([output["score"] for output in sentiment_analyser(response)]))
-    # label_best_of.append(torch.tensor([output["label"] for output in sentiment_analyser(response)]))
+test_data = dataset.map(lambda sample: {
+    "query": sample["query"],
+    "label": sample["label"],
+    "input_ids": sample["input_ids"],
+})
+test_data = test_data.add_column("response_ref", response_tensors_ref)
+test_data = test_data.add_column("response_ref_len",response_tensors_ref_len,)
+test_data = test_data.add_column("response_best_of", response_tensors_best_of)
+test_data = test_data.add_column("response_best_of_len",response_tensors_best_of_len,)
   
-scores_ref = [output["score"] for output in sentiment_analyser(response_tensors_ref)]
-emotion_ref = [output["label"] for output in sentiment_analyser(response_tensors_ref)]  
-# print(dataset['label'])
-# label = [emotion_labels[emo] for emo in dataset['label']]
+rewards: list = [reward(batch) for batch in DataLoader(test_data, batch_size=10)]
+scores_ref = rewards[0]
 
 output_data = dict()
-output_data["query"] = dataset["query"]
 import pandas as pd
+output_data["query"] = dataset['query']
 output_data["label"] = dataset['label']
 output_data["response (ref)"] = response_tensors_ref
-output_data["emotion (ref)"] = emotion_ref
+# output_data["emotion (ref)"] = emotion_ref
 output_data["scores (ref)"] = scores_ref
-output_data["response (best_of)"] = [
-    response_tensors_best_of[i][a.argmax().item()] for i, a in enumerate(scores_best_of)
-]
-output_data["scores (best_of)"] = [a.max().item() for a in scores_best_of]
+# output_data["response (best_of)"] = [
+#     response_tensors_best_of[i][a.argmax().item()] for i, a in enumerate(scores_best_of)
+# ]
+# output_data["scores (best_of)"] = [a.max().item() for a in scores_best_of]
 df_results = pd.DataFrame(output_data)
 print(df_results)
