bffd38b089518296106d6e54ce3f25ed4eb10453
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-13 10:01:35 +0800

    chore: finish optimizing

--- chat-bot/models/sentiment_analysis/libs/CommonConfig.py
+++ chat-bot/models/sentiment_analysis/libs/CommonConfig.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch.cuda
 import wandb
 from dotenv import load_dotenv
+from pyarrow import Field
 from transformers.hf_argparser import HfArg
 
 from .CommonUtils import value_candidate_check
@@ -15,9 +16,9 @@ from .CommonUtils import value_candidate_check
 
 @dataclass
 class CommonScriptArguments:
-    huggingface_api_token: Optional[str] = (
+    huggingface_api_token: Field[Optional[str]] = (
         HfArg(aliases=["--huggingface-api-token", "--huggingface-token", "--hf-token"], default=None))
-    wandb_api_token: Optional[str] = (
+    wandb_api_token: Field[Optional[str]] = (
         HfArg(aliases=["--wandb-api-token", "--wandb-token"], default=None))
 
     def __post_init__(self):
@@ -41,19 +42,23 @@ class CommonScriptArguments:
 
 @dataclass
 class CommonWanDBArguments:
-    name: Optional[str] = HfArg(aliases=["--wandb-name"], default=None)
-    job_type: Optional[str] = HfArg(aliases=["--wandb-job-type", "--job-type"], default=None)
-    config: Union[Dict, str, None] = HfArg(aliases="--wandb-type", default_factory=dict)
-    project: Optional[str] = HfArg(aliases="--wandb-project", default=None)
-    group: Optional[str] = HfArg(aliases=["--wandb-group", "--group"], default=None)
-    notes: Optional[str] = HfArg(aliases=["--wandb-notes", "--notes"], default=None)
-    mode: Optional[Union[Literal["online", "offline", "disabled"], None]] = HfArg(aliases="--wandb-mode", default=None)
-    allow_val_change: Optional[bool] = HfArg(aliases="--allow-val-change", default=False)
-    resume: Optional[str] = HfArg(aliases="--wandb-resume", default=None)
+    name: Field[Optional[str]] = HfArg(aliases=["--wandb-name"], default=None)
+    job_type: Field[Optional[str]] = HfArg(aliases=["--wandb-job-type", "--job-type"], default=None)
+    config: Field[Union[Dict, str, None]] = HfArg(aliases="--wandb-type", default_factory=dict)
+    project: Field[Optional[str]] = HfArg(aliases="--wandb-project", default=None)
+    group: Field[Optional[Union[Literal[
+    				"Sentiment Analysis",
+                    "Emotion Predictor",
+                    "Emotion Model",
+                    "Similarity Analysis",
+                    "Response Generator"], None]]] = HfArg(aliases=["--wandb-group", "--group"], default=None)
+    notes: Field[Optional[str]] = HfArg(aliases=["--wandb-notes", "--notes"], default=None)
+    mode: Field[Optional[Union[Literal["online", "offline", "disabled"], None]]] = HfArg(aliases="--wandb-mode", default=None)
+    allow_val_change: Field[Optional[bool]] = HfArg(aliases="--allow-val-change", default=False)
+    resume: Field[Optional[str]] = HfArg(aliases="--wandb-resume", default=None)
 
     def __post_init__(self):
         module: List = ["Sentiment Analysis",
-                        "Candidate Generator",
                         "Emotion Predictor",
                         "Emotion Model",
                         "Similarity Analysis",
--- chat-bot/models/similarity_analysis/SimilarityAnalyser.py
+++ chat-bot/models/similarity_analysis/SimilarityAnalyser.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Annotated, Any, Callable, Optional, Union
 
 import torch
-from torch import float32, tensor
+from torch import float32, Tensor
 
 
 @dataclass
@@ -18,74 +18,74 @@ class ValueRange:
 
 class SimilarityAnalyser:
     def __init__(self, threshold: Annotated[float, ValueRange(0, 1)], dtype: Optional[Any] = float32,
-                 device: Optional[str] = "cpu") -> None:
+                 device: str = "cpu") -> None:
         self.__threshold: float = ValueRange(0, 1).validate(threshold)
         self.__dtype: Any = dtype
         self.__device: str = device
-        self.__cached_representations: tensor = None
-        self.__cached_ideal_representation: tensor = None
-        self.__cached_similarity: tensor = None
+        self.__cached_representations: Optional[Tensor] = None
+        self.__cached_ideal_representation: Optional[Tensor] = None
+        self.__cached_similarity: Optional[Tensor] = None
 
     @property
     def threshold(self) -> float:
         return self.__threshold
 
     @threshold.setter
-    def threshold(self, new_threshold: Annotated[float, ValueRange(0, 1)]) -> tensor:
+    def threshold(self, new_threshold: Annotated[float, ValueRange(0, 1)]) -> Tensor:
         self.__threshold = ValueRange(0, 1).validate(new_threshold)
         self.__call__(self.__cached_representations, self.__cached_ideal_representation)
 
-    def __calculate_ratio_of_length_of_representation(self) -> tensor:
-        length_of_representations: tensor = torch.norm(self.__cached_representations, dim=1)
-        length_of_ideal_representation: tensor = torch.norm(self.__cached_ideal_representation)
+    def __calculate_ratio_of_length_of_representation(self) -> Tensor:
+        length_of_representations: Tensor = torch.norm(self.__cached_representations, dim=1)
+        length_of_ideal_representation: Tensor = torch.norm(self.__cached_ideal_representation)
 
         return length_of_representations / length_of_ideal_representation
 
     def __call__(
             self,
-            representations: Union[list[tensor], tensor],
-            ideal_representation: tensor
-    ) -> tensor:
+            representations: Union[list[Tensor], Tensor],
+            ideal_representation: Tensor
+    ) -> Tensor:
 
         self.__cached_representations = (
-            representations if type(representations) is tensor else torch.stack(representations))
+            representations if type(representations) is Tensor else torch.stack(representations))
         self.__cached_representations = (
             self.__cached_representations.clone().detach().to(dtype=self.__dtype, device=self.__device))
 
         self.__cached_ideal_representation = (
             ideal_representation.clone().detach().to(dtype=self.__dtype, device=self.__device))
 
-        cosine_similarity: tensor = torch.cosine_similarity(self.__cached_representations,
+        cosine_similarity: Tensor = torch.cosine_similarity(self.__cached_representations,
                                                             self.__cached_ideal_representation)
-        ratio_of_representations: tensor = self.__calculate_ratio_of_length_of_representation()
+        ratio_of_representations: Tensor = self.__calculate_ratio_of_length_of_representation()
         self.__cached_similarity = torch.clamp(cosine_similarity * ratio_of_representations, 0, 1)
 
         return self.__cached_similarity
 
     @staticmethod
-    def __get_indices_of_filtered_tensor(target_tensor: tensor,
-                                         filter_func: Callable[[tensor], tensor]
-                                         ) -> tensor:
-        mask: tensor = filter_func(target_tensor)
+    def __get_indices_of_filtered_tensor(target_tensor: Tensor,
+                                         filter_func: Callable[[Tensor], Tensor]
+                                         ) -> Tensor:
+        mask: Tensor = filter_func(target_tensor)
 
         return torch.nonzero(mask)
 
     def get_max_similarity(self) -> float:
-        valid_similarity_indices: tensor = self.__get_indices_of_filtered_tensor(self.__cached_similarity,
+        valid_similarity_indices: Tensor = self.__get_indices_of_filtered_tensor(self.__cached_similarity,
                                                                                  lambda x: x <= self.__threshold)
         if len(valid_similarity_indices) == 0:
             return 0
 
-        valid_similarity: tensor = self.__cached_similarity[valid_similarity_indices]
+        valid_similarity: Tensor = self.__cached_similarity[valid_similarity_indices]
         return torch.max(valid_similarity).unique().item()
 
     def get_representation_with_max_similarity(self, max_similarity: float) -> list:
-        representation_with_max_similarity_indices: tensor = (
+        representation_with_max_similarity_indices: Tensor = (
             self.__get_indices_of_filtered_tensor(self.__cached_similarity, lambda x: x == max_similarity))
 
         return self.__cached_representations[representation_with_max_similarity_indices].squeeze().tolist()
 
-    def get_most_similar_representation(self) -> dict[tensor, float]:
+    def get_most_similar_representation(self) -> dict[Tensor, float]:
         max_similarity: float = self.get_max_similarity()
         representations_with_max_similarity: list = self.get_representation_with_max_similarity(max_similarity)
 
