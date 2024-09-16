d0bdff4b3f5308161c4b25c6cb5de0ebf09950da
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-16 23:41:27 +0800

    WIP

--- src/models/libs/EmotionChatBot.py
+++ src/models/libs/EmotionChatBot.py
@@ -2,29 +2,89 @@ import torch
 from EmotionModel import EmotionModel
 from ResponseGeneratorPipeline import ResponseGeneratorPipeline
 from SimilarityAnalyser import SimilarityAnalyser
-from transformers import BitsAndBytesConfig
-from transformers.pipelines.text_classification import TextClassificationPipeline
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    pipeline,
+)
+from unsloth import FastLanguageModel
 
 
 class EmotionChatBot:
     def __init__(self, threshold: float) -> None:
-        self.__response_generator = ResponseGeneratorPipeline()
-        self.__sentiment_analyzer = TextClassificationPipeline(
-            model="Shotaro30678/sentiment_analysis_for_emotion_chat_bot",
-            framework="pt",
-            task="sentiment-analysis",
-            num_workers=16,
+	   	model, tokenizer = FastLanguageModel.from_pretrained(
+	        model_name="Shotaro30678/response_generator_DPO",  # YOUR MODEL YOU USED FOR TRAINING
+	        attn_implementation="flash_attention_2",
+		    pretraining_tp=1,
+		    load_in_4bit=True,
+		    device_map="auto",
+		    low_cpu_mem_usage=True,
+		    trust_remote_code=True,
+	    )
+
+		FastLanguageModel.for_inference(model)
+
+		self.__response_generator = ResponseGeneratorPipeline(
+			model,
+		    tokenizer,
+		    framework="pt",
+		    task="conversation-generation",
+		    num_workers=16,
+		    torch_dtype="auto",
+		    add_special_tokens=True,
+		    truncation=False,
+		    padding=True,
+		)
+        sentiment_analysis_model = AutoModelForSequenceClassification.from_pretrained(
+            "Shotaro30678/sentiment_analysis_for_emotion_chat_bot",
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+            ),
             device_map="auto",
-            torch_dtype="auto",
-            model_kwargs={
-                "quantization_config": BitsAndBytesConfig(
-                    load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
-                ),
-                "low_cpu_mem_usage": True,
-            },
+            low_cpu_mem_usage=True,
+        )
+
+        sentiment_analysis_tokenizer = AutoTokenizer.from_pretrained(
+            "Shotaro30678/sentiment_analysis_for_emotion_chat_bot",
             trust_remote_code=True,
         )
-        self.__emotion_predictor = TextClassificationPipeline()
+
+        sentiment_analysis_model = torch.compile(sentiment_analysis_model)
+        self.__sentiment_analyzer = pipeline(
+            "sentiment-analysis",
+            model=sentiment_analysis_model,
+            tokenizer=sentiment_analysis_tokenizer,
+            top_k=7,
+            torch_dtype=torch.float32,
+            device_map="auto",
+            trust_remote_code=True,
+        )
+
+        emotion_predictor_model = AutoModelForSequenceClassification.from_pretrained(
+            "Shotaro30678/sentiment_analysis_for_emotion_chat_bot",
+            quantization_config=BitsAndBytesConfig(
+                load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+            ),
+            device_map="auto",
+            low_cpu_mem_usage=True,
+        )
+
+        emotion_predictor_tokenizer = AutoTokenizer.from_pretrained(
+            "Shotaro30678/sentiment_analysis_for_emotion_chat_bot",
+            trust_remote_code=True,
+        )
+
+        emotion_predictor_model = torch.compile(sentiment_analysis_model)
+        self.__emotion_predictor = pipeline(
+            "sentiment-analysis",
+            model=emotion_predictor_model,
+            tokenizer=emotion_predictor_tokenizer,
+            top_k=7,
+            torch_dtype=torch.float32,
+            device_map="auto",
+            trust_remote_code=True,
+        )
         self.__emotion_model = EmotionModel.from_pretrained(
             "hermeschen1116/emotion_model_for_emotion_chat_bot"
         )
