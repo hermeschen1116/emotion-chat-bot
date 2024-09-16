217500f77189705421d58d43cf6e3040dc7cc50d
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-16 09:15:21 +0800

    WIP

--- src/models/libs/EmotionChatBot.py
+++ src/models/libs/EmotionChatBot.py
@@ -1,1 +1,11 @@
+from transformers.pipelines.text_classification import TextClassificationPipeline
+
+from models.libs.EmotionModel import EmotionModel
+from .ResponseGeneratorPipeline import ResponseGeneratorPipeline
+
 class EmotionChatBot:
+	def __init__(self) -> None:
+		self.__response_generator = ResponseGeneratorPipeline()
+		self.__sentiment_analyzer = TextClassificationPipeline()
+		self.__emotion_predictor = TextClassificationPipeline()
+		self.__emotion_model = EmotionModel.from_pretrained()
