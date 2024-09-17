5420ffebf15ca72396ff5093f6361771316b4534
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-17 15:59:34 +0800

    fix: remove unused function

--- src/models/libs/SimilarityAnalyser.py
+++ src/models/libs/SimilarityAnalyser.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Optional, Union
+from typing import Any, Optional, Union
 
 import torch
 from torch import Tensor, float32
@@ -64,66 +64,11 @@ class SimilarityAnalyser:
         cosine_similarity: Tensor = torch.cosine_similarity(
             self.__cached_representations, self.__cached_ideal_representation
         )
-        ratio_of_representations: Tensor = (
-            self.__calculate_ratio_of_length_of_representation()
-        )
+        ratio_of_representations: (
+            Tensor
+        ) = self.__calculate_ratio_of_length_of_representation()
         self.__cached_similarity = torch.clamp(
             cosine_similarity * ratio_of_representations, 0, 1
         )
 
         return self.__cached_similarity
-
-    @staticmethod
-    def __get_indices_of_filtered_tensor(
-        target_tensor: Tensor, filter_func: Callable[[Tensor], Tensor]
-    ) -> Tensor:
-        mask: Tensor = filter_func(target_tensor)
-
-        return torch.nonzero(mask)
-
-    def get_max_similarity(self) -> float:
-        valid_similarity_indices: Tensor = self.__get_indices_of_filtered_tensor(
-            self.__cached_similarity, lambda x: x <= self.__threshold
-        )
-        if len(valid_similarity_indices) == 0:
-            return 0
-
-        valid_similarity: Tensor = self.__cached_similarity[valid_similarity_indices]
-        return torch.max(valid_similarity).unique().item()
-
-    def get_representation_with_max_similarity(self, max_similarity: float) -> list:
-        representation_with_max_similarity_indices: Tensor = (
-            self.__get_indices_of_filtered_tensor(
-                self.__cached_similarity, lambda x: x == max_similarity
-            )
-        )
-
-        return (
-            self.__cached_representations[representation_with_max_similarity_indices]
-            .squeeze()
-            .tolist()
-        )
-
-    def get_most_similar_representation(self) -> dict[Tensor, float]:
-        max_similarity: float = self.get_max_similarity()
-        representations_with_max_similarity: list = (
-            self.get_representation_with_max_similarity(max_similarity)
-        )
-
-        return {
-            "representations": representations_with_max_similarity,
-            "similarity": max_similarity,
-        }
-
-    def get_most_similar_representation_index(self) -> int:
-        max_similarity_value: float = self.get_most_similar_representation()[
-            "similarity"
-        ]
-
-        return (
-            self.__get_indices_of_filtered_tensor(
-                self.__cached_similarity, lambda x: x == max_similarity_value
-            )
-            .squeeze()
-            .tolist()
-        )
