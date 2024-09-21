26543074699be4e30a2f698cc5cc63644cc4807e
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-09-21 15:44:29 +0800

    style: format code

--- src/model/main.py
+++ src/model/main.py
@@ -5,12 +5,12 @@ from model.libs.EmotionChatBot import EmotionChatBot
 # get, post, patch, delete
 app = FastAPI()
 
+
 @app.on_event("startup")
 def initialize():
 	bot = EmotionChatBot()
 
+
 @app.get("/")
 def check_availability() -> bool:
 	return True
-
-def
