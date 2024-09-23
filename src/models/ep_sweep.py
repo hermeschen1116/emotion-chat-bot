f6c7e28a76e56089250adb4fb32d6a8a6f38a964
yangyx30678 <yangyx30678@gmail.com>
2024-09-23 21:47:27 +0800

    feat: add ADASYN for oversampling

--- src/models/ep_sweep.py
+++ src/models/ep_sweep.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser
+from collections import Counter
 
 import numpy as np
 import torch
-from datasets import load_dataset
+from datasets import Dataset, load_dataset
+from imblearn.over_sampling import ADASYN
 from libs.CommonConfig import CommonScriptArguments, CommonWanDBArguments
 from libs.CommonUtils import login_to_service
 from libs.DataProcess import throw_out_partial_row_with_a_label
@@ -23,7 +25,6 @@ config = config_getter.parse_args()
 parser = HfArgumentParser((CommonScriptArguments, CommonWanDBArguments))
 args, wandb_args = parser.parse_json_file(config.json_file)
 
-# [TODO] make this config a json
 # Define sweep configuration
 sweep_configuration = wandb_args.config["sweep_config"]
 sweep_id = wandb.sweep(sweep=sweep_configuration, project=wandb_args.project)
@@ -116,6 +117,39 @@ def main():
 	)
 	validation_dataset.set_format("torch")
 
+	X_train = train_dataset["input_ids"]
+	y_train = train_dataset["label"]
+
+	X_train_np = X_train.numpy()
+	y_train_np = y_train.numpy()
+
+	adasyn = ADASYN(sampling_strategy="auto", n_jobs=-1)
+	X_resampled_np, y_resampled_np = adasyn.fit_resample(X_train_np, y_train_np)
+
+	X_resampled = torch.tensor(X_resampled_np)
+	y_resampled = torch.tensor(y_resampled_np)
+
+	new_train_dataset = []
+
+	for input_id, label in zip(X_resampled, y_resampled):
+		new_train_dataset.append(
+			{
+				"input_ids": input_id,
+				"label": label,
+			}
+		)
+	train_dataset_resampled = Dataset.from_list(new_train_dataset)
+	train_dataset_resampled.set_format("torch")
+	# Take small portion of the data
+	train_dataset_resampled = train_dataset_resampled.shuffle().take(7000)
+	# 計算每個類別的數量
+	class_dist = wandb.Table(columns=["Class", "Count"])
+	labels = train_dataset_resampled["label"].tolist()
+	label_counts = Counter(labels)
+
+	for label, count in label_counts.items():
+		class_dist.add_data(emotion_labels[label], count)
+
 	y = train_dataset["label"].tolist()
 	class_weights = compute_class_weight(class_weight="balanced", classes=np.unique(y), y=y)
 
@@ -154,6 +188,7 @@ def main():
 				"Weighted-F1-per-class-bar": wandb.plot.bar(
 					table, "Class", "Weighted F1", title="Weighted F1 Score per Class"
 				),
+				"Class Distribution": wandb.plot.bar(class_dist, "Class", "Count", title="Class Distribution"),
 			}
 		)
 
@@ -241,7 +276,7 @@ def main():
 		model=base_model,
 		args=trainer_arguments,
 		compute_metrics=compute_metrics,
-		train_dataset=train_dataset,
+		train_dataset=train_dataset_resampled,
 		eval_dataset=validation_dataset,
 		tokenizer=tokenizer,
 	)
