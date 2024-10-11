93831c960569a557806041f769b4d69d669aa4a2
yangyx30678 <yangyx30678@gmail.com>
2024-10-11 16:22:51 +0800

    refactor: align code style with old EP

--- src/scripts/emotion_predictor/fine_tune_llama.py
+++ src/scripts/emotion_predictor/fine_tune_llama.py
@@ -1,86 +1,62 @@
 from argparse import ArgumentParser
-from dataclasses import Field, dataclass
 
 import torch
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, classification_report
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, BitsAndBytesConfig, HfArgumentParser
-from transformers.hf_argparser import HfArg
 
 import wandb
 from emotion_chat_bot.utils.CommonConfig import CommonScriptArguments, CommonWanDBArguments
+from emotion_chat_bot.utils.CommonUtils import login_to_service
 
-
-@dataclass
-class ScriptArguments(CommonScriptArguments):
-	chat_template_file: Field[str] = HfArg(aliases="--chat-template-file", default="")
-
+login_to_service()
 
 config_getter = ArgumentParser()
 config_getter.add_argument("--json_file", required=True, type=str)
 config = config_getter.parse_args()
 
-parser = HfArgumentParser((ScriptArguments, CommonWanDBArguments))
+parser = HfArgumentParser((CommonScriptArguments, CommonWanDBArguments))
 args, wandb_args = parser.parse_json_file(config.json_file)
 
-# Initialize Wandb
-run = wandb.init(
-	name=wandb_args.name,
-	job_type=wandb_args.job_type,
-	config=wandb_args.config,
-	project=wandb_args.project,
-	group=wandb_args.group,
-	notes=wandb_args.notes,
-	mode="offline",
-	resume=wandb_args.resume,
-)
+run = wandb.init(config=wandb_args.config, project=wandb_args.project, group=wandb_args.group)
 
-# Load Dataset
-dataset = load_dataset("Shotaro30678/daily_dialog_for_SA", split="test", num_proc=16, trust_remote_code=True)
-
+dataset = load_dataset("Shotaro30678/daily_dialog_for_EP", split="test", num_proc=16, trust_remote_code=True)
 emotion_labels: list = dataset.features["label"].names
-
-system_prompt: list = [{"role": "system", "content": {"emotion": "", "dialog": run.config["system_prompt"]}}]
-
+num_emotion_labels: int = len(emotion_labels)
 
 quantization_config = BitsAndBytesConfig(
 	load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_use_double_quant=True, bnb_4bit_compute_dtype=torch.bfloat16
 )
 
-
-base_model = AutoModelForSequenceClassification.from_pretrained(
-	run.config["tokenizer"], quantization_config=quantization_config, num_labels=4, device_map="auto"
-)
-
-
-tokenizer = AutoTokenizer.from_pretrained(run.config["tokenizer"], add_prefix_space=True)
-
+tokenizer = AutoTokenizer.from_pretrained(run.config["base_model"], add_prefix_space=True)
 tokenizer.pad_token_id = tokenizer.eos_token_id
 tokenizer.pad_token = tokenizer.eos_token
 
-# tokenizer.padding_side = "right"
-# tokenizer.clean_up_tokenization_spaces = True
-# tokenizer.chat_template = run.config["chat_template"]
-# tokenizer.add_special_tokens(run.config["special_tokens"])
-# base_model.resize_token_embeddings(len(tokenizer))
+base_model = AutoModelForSequenceClassification.from_pretrained(
+	run.config["base_model"],
+	num_labels=num_emotion_labels,
+	id2label={k: v for k, v in enumerate(emotion_labels)},
+	label2id={v: k for k, v in enumerate(emotion_labels)},
+	use_cache=False,
+	device_map="auto",
+	low_cpu_mem_usage=True,
+	trust_remote_code=True,
+)
 
-
-lora_config = LoraConfig(
+peft_config = LoraConfig(
 	r=run.config["lora_rank"],
 	lora_alpha=run.config["lora_alpha"],
 	target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
 	lora_dropout=0.1,
 	bias="none",
 	task_type="SEQ_CLS",
-	# init_lora_weights=run.config["init_lora_weights"],
-	# modules_to_save=["lm_head", "embed_tokens"],
-	# use_rslora=True,
+	init_lora_weights=run.config["init_lora_weights"],
+	use_rslora=run.config["use_rslora"],
 )
 
-
 base_model = prepare_model_for_kbit_training(base_model)
-base_model = get_peft_model(base_model, lora_config)
+base_model = get_peft_model(base_model, peft_config)
 
 base_model.config.pad_token_id = tokenizer.pad_token_id
 base_model.config.use_cache = False
